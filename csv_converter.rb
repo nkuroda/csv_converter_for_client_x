c72f8bc15f7208ea875d8ebd52813cfc3eb39f79
Kuroda.Naoki <kuroda.naoki@moneyforward.co.jp>
2018-11-25 23:15:38 +0900

    指定のフォルダにあるCSVファイルをコンバート処理にかけて保存し、バックアップ処理を行うClass `ClientX::Executor` を追加

--- csv_converter.rb
+++ csv_converter.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'yaml'
 
 module ClientX
   # 以下のClassは税抜合計金額(19番目のカラム)、税込合計金額(20番目のカラム)、消費税合計額(21番目のカラム)が、
@@ -45,6 +46,38 @@ module ClientX
         headers: nil,
         converters: nil
       )
+    end
+  end
+
+  # 指定のフォルダにあるCSVファイルをコンバート処理にかけて保存し、バックアップ処理を行うClass
+  class Executor
+    def initialize
+      @config = YAML.load_file('config.yml')
+      @source_dir = File.expand_path(@config['source_dir'])
+      @dist_dir = File.expand_path(@config['dist_dir'])
+      @backup_dir = File.expand_path(@config['backup_dir'])
+    end
+
+    def execute!
+      target_file_paths = Dir.glob("#{@source_dir}/*.csv")
+      target_file_paths.each do |target_file_path|
+        target_file_name = File.basename(target_file_path)
+        # まず変換後のCSVを取得し、dist_pathにファイル出力する
+        converted_csv = ClientX::CsvConverter.new(file_path: target_file_path).convert!
+        # prefix に `converted_` を付加したファイル名で保存する
+        dist_file_name = "converted_#{target_file_name}"
+        dist_file_path = File.expand_path(dist_file_name, @dist_dir)
+        CSV.open(dist_file_path, 'w', encoding: 'CP932', force_quotes: true) do |csv|
+          converted_csv.each do |csv_row|
+            csv << csv_row
+          end
+        end
+        # 処理完了したファイルはbackup_dirに移動する
+        # prefix に `backuped_` を付加したファイル名で保存する
+        backup_file_name = "backuped_#{target_file_name}"
+        backup_file_path = File.expand_path(backup_file_name, @backup_dir)
+        File.rename(target_file_path, backup_file_path)
+      end
     end
   end
 end
